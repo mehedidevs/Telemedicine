83191bd751dee8ad9afb390aa3f619cf064abe5d
Mehedi <mehedi.hasan.app.dev@gmail.com>
2023-01-29 18:51:33 +0600

    Cropper Library added

--- app/src/main/java/com/ju/telemedicineju/MainActivity.kt
+++ app/src/main/java/com/ju/telemedicineju/MainActivity.kt
@@ -2,11 +2,18 @@ package com.ju.telemedicineju
 
 import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.internal.ContextUtils.getActivity
 import com.ju.telemedicineju.databinding.ActivityMainBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +23,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
+
         setContentView(binding.root)
+
+
+        takePermission()
 
 //        val frg = binding.fragmentContainerView.getFragment<Fragment>()
 //
@@ -32,5 +44,37 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+
+    private fun takePermission() {
+        Dexter.withContext(this)
+            .withPermissions(
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    if (report.areAllPermissionsGranted()){
+
+//                        location = getLastLocation()
+//
+//                        if (location != null){
+//                            lat = location!!.latitude
+//                            lng = location!!.longitude
+//
+//                            Log.d("TAG", "onPermissionsChecked:  $lat $lng", )
+//                            Toast.makeText(context,"lat: $lat  and  lng: $lng", Toast.LENGTH_LONG).show()
+//                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest?>?,
+                    token: PermissionToken?
+                ) { /* ... */
+                }
+            }).check()
     }
 }
--- app/src/main/java/com/ju/telemedicineju/register/DoctorFragment.kt
+++ app/src/main/java/com/ju/telemedicineju/register/DoctorFragment.kt
@@ -1,13 +1,107 @@
 package com.ju.telemedicineju.register
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
+import com.canhub.cropper.CropImage
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.CropImageOptions
 import com.ju.telemedicineju.R
+import com.ju.telemedicineju.databinding.FragmentDoctorBinding
+import com.ju.telemedicineju.databinding.FragmentStartBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DoctorFragment : Fragment() {
+
+
+    private var _binding: FragmentDoctorBinding? = null
+    private val binding get() = _binding!!
+
+    private var outputUri: Uri? = null
+
+    private val cropImage = registerForActivityResult(CropImageContract()) { result ->
+        when {
+            result.isSuccessful -> {
+                handleCropImageResult(result.uriContent.toString())
+            }
+            result is CropImage.CancelledResult -> showErrorMessage("cropping image was cancelled by the user")
+            else -> showErrorMessage("cropping image failed")
+        }
+    }
+
+    private val customCropImage = registerForActivityResult(CropImageContract()) {
+        if (it !is CropImage.CancelledResult) {
+            handleCropImageResult(it.uriContent.toString())
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.takePhoto.setOnClickListener {
+            startCameraWithoutUri(includeCamera = true, includeGallery = true)
+        }
+
+
+    }
+
+
+    private fun startCameraWithoutUri(includeCamera: Boolean, includeGallery: Boolean) {
+        customCropImage.launch(
+            CropImageContractOptions(
+                uri = null,
+                cropImageOptions = CropImageOptions(
+                    imageSourceIncludeCamera = includeCamera,
+                    imageSourceIncludeGallery = includeGallery,
+                ),
+            ),
+        )
+    }
+
+    private fun startCameraWithUri() {
+        cropImage.launch(
+            CropImageContractOptions(
+                uri = outputUri,
+                cropImageOptions = CropImageOptions(),
+            ),
+        )
+    }
+
+    private fun showErrorMessage(message: String) {
+
+        Toast.makeText(activity, "Crop failed: $message", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun handleCropImageResult(uri: String) {
+        var mUri=Uri.parse(uri)
+
+
+        binding.takePhoto.setImageURI(mUri)
+
+        //  SampleResultScreen.start(this, null, Uri.parse(uri.replace("file:", "")), null)
+    }
+
+
+
+
+
 
 
     override fun onCreateView(
@@ -15,7 +109,11 @@ class DoctorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor, container, false)
+        _binding = FragmentDoctorBinding.inflate(layoutInflater, container, false)
+
+
+
+        return binding.root
     }
 
 
